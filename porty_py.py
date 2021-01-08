844b3398ec0b7576a6bbc4dc8bcbdaeceddce2c9
JiteshKanojia <jiteshkanojia22@gmail.com>
2021-01-08 16:25:58 +0530

    Improved Script for windows

--- porty_py.py
+++ porty_py.py
@@ -1,9 +1,8 @@
+import os
 import socket
-import subprocess
-import datetime 
+import datetime
 
-#clear the shell 
-subprocess.call("clear",shell=True) 
+os.system("clear||cls")
 
 print("-"*60)
 
@@ -13,22 +12,44 @@ start_time = datetime.datetime.now()
 print("scanning for IP: "+str(ip)+"\n"+"-"*60 + "\n")
 print("port\tstatus\tservice" +"\n----\t------\t-------")
 
-max_range = 65535
+#Max port number to scan upto
+max_range = 2000
 
 
 
-def portScan(ip):
+def portScan(ip,max_range):
     
     for port in range(1,max_range): # For scanning ports max range is (2^16 - 1) = 65535 
         try: 
             s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
             s.connect((ip,port))
-            service_name = socket.getservbyport(port,)
+            service_name = socket.getservbyport(port,'tcp')
             print(str(port) + "\topen\t"+str(service_name))
             s.close()
-        except socket.error:pass
-       
-portScan(ip)
+        except socket.error:
+            pass
+
+#For windows connection timeout needs to be declared
+#Default set to 1 us
+timeout = 0.000001
+
+def portScanWin(ip,max_range,timeout):
+    
+    for port in range(1,max_range): # For scanning ports max range is (2^16 - 1) = 65535 
+        try: 
+            s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            s.settimeout(timeout)
+            s.connect((ip,port))
+            service_name = socket.getservbyport(port,'tcp')
+            print(str(port) + "\topen\t"+str(service_name))
+            s.close()
+        except socket.error:
+            pass
+
+if(os.name=='nt'):
+    portScanWin(ip,max_range,timeout)
+else:
+    portScan(ip,max_range)
 
 print("\n"+"-"*60)
 
